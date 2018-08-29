41e30be2518abd9549b8b46d0a07f05e07ebecc2
fred <domaine75@gmail.com>
2018-08-29 09:01:53 +0200

    Fix a bug

--- UnitTestProjectMyProjectEulerSolutions/UnitTestFormatResult.cs
+++ UnitTestProjectMyProjectEulerSolutions/UnitTestFormatResult.cs
@@ -1,5 +1,5 @@
-﻿using Problem31;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problem31;
 
 namespace UnitTestProjectMyProjectEulerSolutions
 {
@@ -7,7 +7,7 @@ namespace UnitTestProjectMyProjectEulerSolutions
   public class UnitTestFormatResult
   {
     [TestMethod]
-    public void TestMethod_1_time200()
+    public void TestMethod_1_time_200()
     {
       const int sourcea = 1;
       const int sourceb = 0;
@@ -18,6 +18,22 @@ namespace UnitTestProjectMyProjectEulerSolutions
       const int sourceg = 0;
       const int sourceh = 0;
       const string expected = "200=200*1";
+      string result = Program.FormatResult(sourcea, sourceb, sourcec, sourced, sourcee, sourcef, sourceg, sourceh);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_2_time_100()
+    {
+      const int sourcea = 0;
+      const int sourceb = 2;
+      const int sourcec = 0;
+      const int sourced = 0;
+      const int sourcee = 0;
+      const int sourcef = 0;
+      const int sourceg = 0;
+      const int sourceh = 0;
+      const string expected = "200=100*2";
       string result = Program.FormatResult(sourcea, sourceb, sourcec, sourced, sourcee, sourcef, sourceg, sourceh);
       Assert.AreEqual(result, expected);
     }
