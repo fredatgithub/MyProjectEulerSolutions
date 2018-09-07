f9f0902cade44f640dd2f1239e3424842f99f9de
fred <domaine75@gmail.com>
2018-09-07 20:10:14 +0200

    Add a new project to create unit test file

---
+++ CreateUnitTestFile/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateUnitTestFile
+{
+  internal class Program
+  {
+    private static void Main()
+    {
+      // read the result file and then create a unit test file
+
+    }
+  }
+}
