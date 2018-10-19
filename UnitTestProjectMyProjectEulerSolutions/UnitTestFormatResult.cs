468aae027e3f475be2b593e19b0abbb4e00d1ec0
fred <domaine75@gmail.com>
2018-10-19 09:45:08 +0200

    Add unit test

--- UnitTestProjectMyProjectEulerSolutions/UnitTestFormatResult.cs
+++ UnitTestProjectMyProjectEulerSolutions/UnitTestFormatResult.cs
@@ -1045,5 +1045,21 @@ namespace UnitTestProjectMyProjectEulerSolutions
       const string expected = "200=5*38+2*5";
       Assert.AreEqual(result, expected);
     }
+
+    [TestMethod]
+    public void TestMethod_thirty_nine()
+    {
+      const int a = 0;    // 200;
+      const int b = 0;    // 100;
+      const int c = 0;    // 50;
+      const int d = 0;    // 20;
+      const int e = 0;    // 10;
+      const int f = 39;   // 5;
+      const int g = 2;    // 2;
+      const int h = 1;    // 1;
+      string result = Program.FormatResult(a, b, c, d, e, f, g, h);
+      const string expected = "200=5*39+2*2+1*1";
+      Assert.AreEqual(result, expected);
+    }
   }
 }
